ee42e21e8007872ab55b4bcd2762954013f3e537
ronywebtricker@gmail.com <ronywebticker@gmail.com>
2022-04-26 13:23:47 +0600

    3D-game

---
+++ js/common-scripts.js
@@ -0,0 +1,22 @@
+
+(function($){
+	$(function(){
+
+
+
+
+        $('.hamburger').click(function () {
+            $("body").toggleClass("navShown");
+            $(".nav-wrap").fadeToggle()
+        });
+        
+		
+	})// End ready function.
+   
+    
+    
+
+	
+
+})(jQuery)
+
